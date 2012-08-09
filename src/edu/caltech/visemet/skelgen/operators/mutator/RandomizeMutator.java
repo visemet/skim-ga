df3821603882e7b43a28effa14b4168de9df2126
Max Hirschhorn <maxh@caltech.edu>
2012-08-09 12:38:12 -0700

    Created RandomizeMutator class.
    Iterates through each gene of the given chromosome, and randomizes the
    base of that gene conditioned on the random value produced by the
    generator.

---
+++ src/edu/caltech/visemet/skelgen/operators/mutator/RandomizeMutator.java
@@ -0,0 +1,30 @@
+package edu.caltech.visemet.skelgen.operators.mutator;
+
+import edu.caltech.visemet.skelgen.Chromosome;
+import edu.caltech.visemet.skelgen.Gene;
+import edu.caltech.visemet.skelgen.MutationOperator;
+import java.util.Random;
+
+/**
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public class RandomizeMutator implements MutationOperator {
+
+    private Random random = new Random();
+
+    @Override
+    public void mutate(double probability, Chromosome chromosome) {
+        int geneLength = chromosome.length();
+        for (int geneIndex = 0; geneIndex < geneLength; geneIndex++) {
+            Gene gene = chromosome.getGeneAt(geneIndex);
+
+            int baseLength = gene.length();
+            for (int baseIndex = 0; baseIndex < baseLength; baseIndex++) {
+                if (random.nextDouble() < probability) {
+                    gene.getBaseAt(baseIndex).randomize(random);
+                }
+            }
+        }
+    }
+}
