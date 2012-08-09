d8bd4fd31574135feff586f54ffa8ffe5dfa9a52
Max Hirschhorn <maxh@caltech.edu>
2012-08-08 18:23:55 -0700

    Created DefaultPopulation class.
    Backed by Chromosome collection.

---
+++ src/edu/caltech/visemet/skelgen/DefaultPopulation.java
@@ -0,0 +1,42 @@
+package edu.caltech.visemet.skelgen;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Random;
+
+/**
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public class DefaultPopulation implements Population {
+
+    private Collection<Chromosome> chromosomes;
+
+    public DefaultPopulation(Collection<Chromosome> chromosomes) {
+        this.chromosomes = chromosomes;
+    }
+
+    @Override
+    public void initialize() {
+        Random random = new Random();
+
+        for (Chromosome chromosome : chromosomes) {
+            chromosome.randomize(random);
+        }
+    }
+
+    @Override
+    public boolean add(Chromosome chromosome) {
+        return chromosomes.add(chromosome);
+    }
+
+    @Override
+    public int size() {
+        return chromosomes.size();
+    }
+
+    @Override
+    public Iterator<Chromosome> iterator() {
+        return chromosomes.iterator();
+    }
+}
