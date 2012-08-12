14b2bff0193bcf93293005ed86efe3050e98f2c0
Max Hirschhorn <maxh@caltech.edu>
2012-08-12 13:40:14 -0700

    Created AbstractGeneticAlgorithm class.
    Evolves next population using the specified configuration of crossover and
    mutation operators. Creates a default population and adds default
    chromosomes with genes determined by the crossover and mutator.
    
    Implementation arbitrarily decides to attempt to crossover the parent at
    a particular index, and the parent in the index next to it, wrapping back
    to the first element when crossing-over the last.

---
+++ src/edu/caltech/visemet/skelgen/AbstractGeneticAlgorithm.java
@@ -0,0 +1,50 @@
+package edu.caltech.visemet.skelgen;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public abstract class AbstractGeneticAlgorithm implements GeneticAlgorithm {
+
+    private GeneticAlgorithmConfiguration configuration;
+
+    public AbstractGeneticAlgorithm(GeneticAlgorithmConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    public Population evolve(FitnessEvaluator evaluator, SelectionOperator selector, Population population) {
+        Population nextPopulation = new DefaultPopulation(new ArrayList<Chromosome>());
+
+        double crossoverProbability = configuration.getCrossoverProbability();
+        double mutationProbability = configuration.getMutationProbability();
+
+        CrossoverOperator crossover = configuration.getCrossover();
+        MutationOperator mutator = configuration.getMutator();
+
+        Chromosome[] chromosomes = selector.select(population.size(), evaluator, population);
+        for (int chromosomeIndex = 0; chromosomeIndex < chromosomes.length; chromosomeIndex++) {
+            Chromosome parent = chromosomes[chromosomeIndex];
+            Chromosome otherParent = chromosomes[(chromosomeIndex + 1) % chromosomes.length];
+
+            int chromosomeLength = Math.min(parent.length(), otherParent.length());
+            Chromosome child = new DefaultChromosome(new Gene[chromosomeLength]);
+
+            for (int geneIndex = 0; geneIndex < chromosomeLength; geneIndex++) {
+                Gene parentGene = parent.getGeneAt(geneIndex);
+                Gene otherParentGene = otherParent.getGeneAt(geneIndex);
+
+                Gene childGene = crossover.crossover(crossoverProbability, parentGene, otherParentGene);
+                mutator.mutate(mutationProbability, childGene);
+
+                child.setGeneAt(geneIndex, childGene);
+            }
+
+            nextPopulation.add(child);
+        }
+
+        return nextPopulation;
+    }
+}
