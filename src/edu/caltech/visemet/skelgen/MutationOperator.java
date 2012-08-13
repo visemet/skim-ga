22a74c36a86f4162ce3dc5771ae08114e50303f1
Max Hirschhorn <maxh@caltech.edu>
2012-08-12 20:13:52 -0700

    Made MutationOperator class generic; fixes #1.
    Changed InversionOperator to enforce that mutate() method acts only on
    instances of BooleanGene.

--- src/edu/caltech/visemet/skelgen/MutationOperator.java
+++ src/edu/caltech/visemet/skelgen/MutationOperator.java
@@ -4,7 +4,7 @@ package edu.caltech.visemet.skelgen;
  *
  * @author Max Hirschhorn #visemet
  */
-public interface MutationOperator {
+public interface MutationOperator<T extends Gene> {
 
-    void mutate(double probability, Gene gene);
+    void mutate(double probability, T gene);
 }
--- src/edu/caltech/visemet/skelgen/operators/mutator/InversionMutator.java
+++ src/edu/caltech/visemet/skelgen/operators/mutator/InversionMutator.java
@@ -1,24 +1,24 @@
 package edu.caltech.visemet.skelgen.operators.mutator;
 
-import edu.caltech.visemet.skelgen.Base;
-import edu.caltech.visemet.skelgen.Gene;
 import edu.caltech.visemet.skelgen.MutationOperator;
+import edu.caltech.visemet.skelgen.genes.BooleanBase;
+import edu.caltech.visemet.skelgen.genes.BooleanGene;
 import java.util.Random;
 
 /**
  *
  * @author Max Hirschhorn #visemet
  */
-public class InversionMutator implements MutationOperator {
+public class InversionMutator implements MutationOperator<BooleanGene> {
 
     private Random random = new Random();
 
     @Override
-    public void mutate(double probability, Gene gene) {
+    public void mutate(double probability, BooleanGene gene) {
         int geneLength = gene.length();
         for (int baseIndex = 0; baseIndex < geneLength; baseIndex++) {
             if (random.nextDouble() < probability) {
-                Base<Boolean> base = gene.getBaseAt(baseIndex);
+                BooleanBase base = gene.getBaseAt(baseIndex);
                 base.setValue(!base.getValue());
             }
         }
