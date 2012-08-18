c899dcf25c42d6cea4a26f028bbfb9888d9e8eb0
Max Hirschhorn <maxh@caltech.edu>
2012-08-18 14:14:59 -0700

    Created OneMaxExample class.
    Trivial example to demonstrate how to create a genetic algorithm using
    this framework. Optimal solution is of course a Gene containing
    BooleanBase instances all with value=true.

---
+++ src/edu/caltech/visemet/skelgen/examples/onemax/OneMaxExample.java
@@ -0,0 +1,165 @@
+package edu.caltech.visemet.skelgen.examples.onemax;
+
+import edu.caltech.visemet.skelgen.*;
+import edu.caltech.visemet.skelgen.algorithms.FixedTerminationGeneticAlgorithm;
+import edu.caltech.visemet.skelgen.genes.BooleanGene;
+import edu.caltech.visemet.skelgen.operators.crossover.NpointCrossover;
+import edu.caltech.visemet.skelgen.operators.mutator.InversionMutator;
+import edu.caltech.visemet.skelgen.operators.selector.RouletteWheelSelector;
+import edu.caltech.visemet.skelgen.statistics.PopulationStatistics;
+import java.util.ArrayList;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+/**
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public class OneMaxExample extends AbstractExample {
+
+    public OneMaxExample(ExampleConfiguration config) {
+        super(config);
+    }
+
+    @Override
+    protected void createEvaluator() {
+        setEvaluator(new OneMaxFitnessFunction());
+    }
+
+    @Override
+    protected void createSelector() {
+        setSelector(new RouletteWheelSelector());
+    }
+
+    @Override
+    protected void createPopulation() {
+        setPopulation(new DefaultPopulation(new ArrayList<Chromosome>()));
+
+        Population population = getPopulation();
+        ExampleConfiguration config = getConfig();
+
+        int geneLength = config.getGeneLength();
+        int populationSize = config.getPopulationSize();
+
+        for (int count = 0; count < populationSize; count++) {
+            population.add(new DefaultChromosome(new Gene[] {
+                new BooleanGene(geneLength)
+            }));
+        }
+    }
+
+    @Override
+    protected void createAlgorithm() {
+        ExampleConfiguration config = getConfig();
+
+        int numGenerations = config.getNumGenerations();
+        double crossoverProbability = config.getCrossoverProbability();
+        double mutationProbability = config.getMutationProbability();
+
+        setAlgorithm(new FixedTerminationGeneticAlgorithm(numGenerations,
+                new GeneticAlgorithmConfiguration.Builder()
+                        .setShouldRetainMostFit(true)
+                        .setCrossoverProbability(crossoverProbability)
+                        .setMutationProbability(mutationProbability)
+                        .setCrossover(NpointCrossover.ONE_POINT)
+                        .setMutator(InversionMutator.INSTANCE)
+                        .build()));
+    }
+
+    @Override
+    public void execute() {
+        super.execute();
+
+        FitnessEvaluator evaluator = getEvaluator();
+        Population population = getPopulation();
+
+        Chromosome mostFit = PopulationStatistics.getMostFitChromosome(evaluator, population);
+        System.out.printf("chromosome: %s\nfitness: %.6f\n", mostFit, evaluator.evaluate(mostFit));
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ParseException {
+        CommandLineParser parser = new PosixParser();
+
+        Options options = new Options();
+
+        Option gene = OptionBuilder
+                .withLongOpt("gene-length")
+                .withDescription("length of gene")
+                .hasArg()
+                .withArgName("LENGTH")
+                .withType(Number.class)
+                .isRequired()
+                .create();
+
+        Option population = OptionBuilder
+                .withLongOpt("population-size")
+                .withDescription("size of population")
+                .hasArg()
+                .withArgName("SIZE")
+                .withType(Number.class)
+                .isRequired()
+                .create();
+
+        Option generations = OptionBuilder
+                .withLongOpt("num-generations")
+                .withDescription("number of generations")
+                .hasArg()
+                .withArgName("NUM")
+                .withType(Number.class)
+                .isRequired()
+                .create();
+
+        Option crossover = OptionBuilder
+                .withLongOpt("crossover-prob")
+                .withDescription("probability of crossover")
+                .hasArg()
+                .withArgName("PROB")
+                .withType(Number.class)
+                .isRequired()
+                .create();
+
+        Option mutation = OptionBuilder
+                .withLongOpt("mutation-prob")
+                .withDescription("probability of mutation")
+                .hasArg()
+                .withArgName("PROB")
+                .withType(Number.class)
+                .isRequired()
+                .create();
+
+        options.addOption(gene);
+        options.addOption(population);
+        options.addOption(generations);
+        options.addOption(crossover);
+        options.addOption(mutation);
+
+        // Parses the command line arguments
+        CommandLine line = parser.parse(options, args);
+
+        int geneLength = (int) (long) line.getParsedOptionValue(gene.getLongOpt());
+        int populationSize = (int) (long) line.getParsedOptionValue(population.getLongOpt());
+        int numGenerations = (int) (long) line.getParsedOptionValue(generations.getLongOpt());
+        double crossoverProbability = (double) line.getParsedOptionValue(crossover.getLongOpt());
+        double mutationProbability = (double) line.getParsedOptionValue(mutation.getLongOpt());
+
+        ExampleConfiguration config = new ExampleConfiguration.Builder()
+                .setGeneLength(geneLength)
+                .setPopulationSize(populationSize)
+                .setNumGenerations(numGenerations)
+                .setCrossoverProbability(crossoverProbability)
+                .setMutationProbability(mutationProbability)
+                .build();
+
+        Example example = new OneMaxExample(config);
+        example.configure();
+        example.execute();
+    }
+}
