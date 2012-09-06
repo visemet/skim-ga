4e9132ac7d11196259e56f64d5bf156d6fc8d9bf
Max Hirschhorn <maxh@caltech.edu>
2012-09-06 00:52:15 -0700

    Overrode copy() method in NumberBase interface.
    Changes type signature to NumberBase<T> instead of Base<T>. However, all
    implementing classes should provide a concrete type for the return type.

--- src/edu/caltech/visemet/skim/NumberBase.java
+++ src/edu/caltech/visemet/skim/NumberBase.java
@@ -6,6 +6,9 @@ package edu.caltech.visemet.skim;
  */
 public interface NumberBase<T extends Number> extends Base<T> {
 
+    @Override
+    NumberBase<T> copy();
+
     T getMinValue();
 
     void setMinValue(T minValue);
