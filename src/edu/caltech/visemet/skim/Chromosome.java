d2d9dd4d651914ad654c823d7f62a1da1fa73ed5
Max Hirschhorn <maxh@caltech.edu>
2012-09-07 09:40:19 -0700

    Updated documentation.
    Added doc comments for all methods and type parameters of Chromosome and
    Population classes.

--- src/edu/caltech/visemet/skim/Chromosome.java
+++ src/edu/caltech/visemet/skim/Chromosome.java
@@ -4,17 +4,52 @@ import java.util.Random;
 
 /**
  *
+ * @param <T> the type of value for bases of genes of this chromosome
+ * @param <S> the type of base for genes of this chromosome
+ * @param <U> the type of gene for this chromosome
+ *
  * @author Max Hirschhorn #visemet
  */
 public interface Chromosome<T, S extends Base<T>, U extends Gene<T, S>> {
 
+    /**
+     * Initializes the value for bases of genes of this chromosome.
+     *
+     * @see Gene#initialize()
+     */
     void initialize();
 
+    /**
+     * Randomizes the value for bases of genes of this chromosome.
+     *
+     * @param random the pseudorandom number generator
+     *
+     * @see Gene#randomize(java.util.Random)
+     */
     void randomize(Random random);
 
+    /**
+     * Returns the gene at the specified position of this chromosome.
+     *
+     * @param index the index of the gene to return
+     *
+     * @return the gene at the specified position of this chromosome.
+     */
     U getGeneAt(int index);
 
+    /**
+     * Replaces the gene at the specified position of this chromosome with the
+     * specified gene.
+     *
+     * @param index the index of the gene to replace
+     * @param base the gene to be stored at specified position
+     */
     void setGeneAt(int index, U gene);
 
+    /**
+     * Returns the length of this chromosome.
+     *
+     * @return the length of this chromosome
+     */
     int length();
 }
--- src/edu/caltech/visemet/skim/Population.java
+++ src/edu/caltech/visemet/skim/Population.java
@@ -1,9 +1,16 @@
 package edu.caltech.visemet.skim;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 /**
+ *
+ * @param <T> the type of value for bases of genes of chromosomes of this
+ * population
+ * @param <S> the type of base for genes of chromosomes of this population
+ * @param <U> the type of gene for chromosomes of this population
+ * @param <V> the type of chromosome for this population
  *
  * @author Max Hirschhorn #visemet
  */
@@ -14,13 +21,43 @@ public interface Population<
         V extends Chromosome<T, S, U>
 > {
 
+    /**
+     * Initializes the value for bases of genes of chromosomes of this
+     * population.
+     *
+     * @see Chromosome#initialize()
+     */
     void initialize();
 
+    /**
+     * Inserts the specified chromosome into this population.
+     *
+     * @param chromosome the chromosome to be stored
+     *
+     * @return @code{true} if this population has changed
+     *
+     * @see Collection#add(java.lang.Object)
+     */
     boolean add(V chromosome);
 
+    /**
+     * Returns the size of this population.
+     *
+     * @return the size of this population
+     */
     int size();
 
+    /**
+     * Returns an iterator over the chromosomes in this population.
+     *
+     * @return an iterator over the chromosomes in this population
+     */
     Iterator<V> iterator();
 
+    /**
+     * Returns a list containing all of the chromosomes in this population.
+     *
+     * @return a list containing all of the chromosomes in this population
+     */
     List<V> toList();
 }
