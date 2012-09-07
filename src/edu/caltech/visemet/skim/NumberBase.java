5d0eff16f056ea79bafba7876cbee7e15e06bf30
Max Hirschhorn <maxh@caltech.edu>
2012-09-07 12:41:04 -0700

    Updated documentation.
    Added doc comments for all methods and type parameters of NumberBase
    class.

--- src/edu/caltech/visemet/skim/NumberBase.java
+++ src/edu/caltech/visemet/skim/NumberBase.java
@@ -2,6 +2,8 @@ package edu.caltech.visemet.skim;
 
 /**
  *
+ * @param <T> the type of value for this base
+ *
  * @author Max Hirschhorn #visemet
  */
 public interface NumberBase<T extends Number> extends Base<T> {
@@ -9,11 +11,31 @@ public interface NumberBase<T extends Number> extends Base<T> {
     @Override
     NumberBase<T> copy();
 
+    /**
+     * Returns the minimum value accepted by this base.
+     *
+     * @return the minimum value accepted by this base
+     */
     T getMinValue();
 
+    /**
+     * Replaces the minimum value of this base with the specified value.
+     *
+     * @param minValue the value to be stored
+     */
     void setMinValue(T minValue);
 
+    /**
+     * Returns the maximum value accepted by this base.
+     *
+     * @return the maximum value accepted by this base
+     */
     T getMaxValue();
 
+    /**
+     * Replaces the maximum value of this base with the specified value.
+     *
+     * @param maxValue the value to be store
+     */
     void setMaxValue(T maxValue);
 }
