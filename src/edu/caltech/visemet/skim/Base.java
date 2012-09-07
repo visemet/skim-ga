1bd3a3b12f8d6050f05abd7a43cde7f6b525aff0
Max Hirschhorn <maxh@caltech.edu>
2012-09-06 17:36:30 -0700

    Updated documentation.
    Added doc comments for all methods and type parameters.

--- src/edu/caltech/visemet/skim/Base.java
+++ src/edu/caltech/visemet/skim/Base.java
@@ -4,15 +4,37 @@ import java.util.Random;
 
 /**
  *
+ * @param <T> the type of value for this base
+ *
  * @author Max Hirschhorn #visemet
  */
 public interface Base<T> {
 
+    /**
+     * Randomizes the value of this base.
+     *
+     * @param random the pseudorandom number generator
+     */
     void randomize(Random random);
 
+    /**
+     * Returns the value of this base.
+     *
+     * @return the value of this base
+     */
     T getValue();
 
+    /**
+     * Replaces the value of this base with the specified value.
+     *
+     * @param value the value to be stored
+     */
     void setValue(T value);
 
+    /**
+     * Returns an identical base with the same value as this.
+     *
+     * @return an identical base with the same value as this.
+     */
     Base<T> copy();
 }
--- src/edu/caltech/visemet/skim/Gene.java
+++ src/edu/caltech/visemet/skim/Gene.java
@@ -5,23 +5,73 @@ import java.util.Random;
 
 /**
  *
+ * @param <T> the type of value for bases of this gene
+ * @param <S> the type of base for this gene
+ *
  * @author Max Hirschhorn #visemet
  */
 public interface Gene<T, S extends Base<T>> {
 
+    /**
+     * Initializes the value for bases of this gene.
+     */
     void initialize();
 
+    /**
+     * Randomizes the value for bases of this gene.
+     *
+     * @param random the pseudorandom number generator
+     * 
+     * @see Base#randomize(java.util.Random)
+     */
     void randomize(Random random);
 
+    /**
+     * Returns the sequence of this gene.
+     *
+     * @return the sequence of this gene.
+     */
     List<S> getSequence();
 
+    /**
+     * Sets the sequence of this gene.
+     *
+     * @param sequence the sequence to be stored
+     */
     void setSequence(List<S> sequence);
 
+    /**
+     * Returns the base at the specified position in the sequence of this gene.
+     *
+     * @param index the index of the base to return
+     *
+     * @return the base at the specified position in the sequence of this gene.
+     */
     S getBaseAt(int index);
 
+    /**
+     * Replaces the base at the specified position in the sequence of this gene
+     * with the specified base.
+     *
+     * @param index the index of the base to replace
+     * @param base the base be stored at specified position
+     */
     void setBaseAt(int index, S base);
 
+    /**
+     * Returns the length of the sequence of this gene.
+     *
+     * @return the length of the sequence of this gene
+     */
     int length();
 
+    /**
+     * Returns an identical gene with the same sequence as this. The sequence is
+     * composed of identical bases with the same value.
+     *
+     * @return an identical gene with the same sequence as this
+     *
+     * @see Base#copy()
+     */
     Gene<T, S> copy();
 }
