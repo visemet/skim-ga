6773388b4609d6721ef199b0bcb613f211c66cc7
Max Hirschhorn <maxh@caltech.edu>
2012-09-12 12:34:11 -0700

    Updated documentation.
    Added doc comments for all methods and type parameters of
    GeneticAlgorithm interface.

--- src/edu/caltech/visemet/skim/GeneticAlgorithm.java
+++ src/edu/caltech/visemet/skim/GeneticAlgorithm.java
@@ -2,6 +2,13 @@ package edu.caltech.visemet.skim;
 
 /**
  *
+ * @param <T> the type of value for bases of genes of chromosomes of this
+ * genetic algorithm
+ * @param <S> the type of base for genes of chromosomes of this genetic
+ * algorithm
+ * @param <U> the type of gene for chromosomes of this genetic algorithm
+ * @param <V> the type of chromosome for this genetic algorithm
+ *
  * @author Max Hirschhorn #visemet
  */
 public interface GeneticAlgorithm<
@@ -11,10 +18,29 @@ public interface GeneticAlgorithm<
         V extends Chromosome<T, S, U>
 > {
 
+    /**
+     * Returns a population containing the children chromosomes evolved by this
+     * genetic algorithm from the specified parent population using specified
+     * selection operator and fitness evaluator.
+     *
+     * @param evaluator the fitness evaluator used by this genetic algorithm
+     * @param selector the selection operator used by this genetic algorithm
+     * @param population the population from which the chromosomes are evolved
+     *
+     * @return a population containing the children chromosomes evolved by this
+     * genetic algorithm from the specified parent population using specified
+     * selection operator and fitness evaluator
+     */
     Population<T, S, U, V> evolve(
             FitnessEvaluator<T, S, U, V> evaluator,
             SelectionOperator<T, S, U, V> selector,
             Population<T, S, U, V> population);
 
+    /**
+     * Returns whether or not this genetic algorithm should continue to evolve.
+     *
+     * @return {@code true} if this genetic algorithm should terminate, and
+     * {@code false} otherwise
+     */
     boolean shouldTerminate();
 }
