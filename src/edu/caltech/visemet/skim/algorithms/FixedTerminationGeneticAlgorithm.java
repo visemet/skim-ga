8698a9790ed33b6c8e27edd2b9ddb7f4b385c3ef
Max Hirschhorn <maxh@caltech.edu>
2012-09-14 13:48:10 -0700

    Updated documentation.
    Added doc comments for the constructor, type parameters and private
    fields.

--- src/edu/caltech/visemet/skim/algorithms/FixedTerminationGeneticAlgorithm.java
+++ src/edu/caltech/visemet/skim/algorithms/FixedTerminationGeneticAlgorithm.java
@@ -4,6 +4,13 @@ import edu.caltech.visemet.skim.*;
 
 /**
  *
+ * @param <T> the type of value for bases of genes of chromosomes of this
+ * genetic algorithm
+ * @param <S> the type of base for genes of chromosomes of this genetic
+ * algorithm
+ * @param <U> the type of gene for chromosomes of this genetic algorithm
+ * @param <V> the type of chromosome for this genetic algorithm
+ *
  * @author Max Hirschhorn #visemet
  */
 public class FixedTerminationGeneticAlgorithm<
@@ -13,15 +20,29 @@ public class FixedTerminationGeneticAlgorithm<
         V extends Chromosome<T, S, U>
 > extends AbstractGeneticAlgorithm<T, S, U, V> {
 
+    /**
+     * Holds the current generation count of this genetic algorithm.
+     */
     private int generationCount;
 
+    /**
+     * Holds the maximum generation count of this genetic algorithm.
+     */
     private int maxGenerationCount;
 
+    /**
+     * Class constructor specifying the maximum generation count and the
+     * configuration.
+     *
+     * @param maxGenerationCount the maximum generation count of this genetic
+     * algorithm
+     * @param config the configuration used by this genetic algorithm
+     */
     public FixedTerminationGeneticAlgorithm(
             int maxGenerationCount,
-            GeneticAlgorithmConfiguration<T, S, U> configuration) {
+            GeneticAlgorithmConfiguration<T, S, U> config) {
 
-        super(configuration);
+        super(config);
         this.maxGenerationCount = maxGenerationCount;
     }
 
