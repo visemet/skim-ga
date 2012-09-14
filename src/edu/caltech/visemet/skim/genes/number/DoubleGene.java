8d56d8689d832f90853182edaf808110ddaf7454
Max Hirschhorn <maxh@caltech.edu>
2012-09-14 16:49:48 -0700

    Created DoubleGene class.
    Initialized by setting each base as a new DoubleBase instance.

---
+++ src/edu/caltech/visemet/skim/genes/number/DoubleGene.java
@@ -0,0 +1,49 @@
+package edu.caltech.visemet.skim.genes.number;
+
+import edu.caltech.visemet.skim.AbstractGene;
+import edu.caltech.visemet.skim.NumberBase;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @param <T> the type of value for bases of this gene
+ * @param <S> the type of base for this gene
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public class DoubleGene<
+        T extends Double,
+        S extends NumberBase<T>
+> extends AbstractGene<T, S> {
+
+    /**
+     * Class constructor specifying the length.
+     *
+     * @param length the length of this gene
+     */
+    public DoubleGene(int length) {
+        super(new ArrayList<>(Collections.nCopies(length, (S) null)));
+    }
+
+    @Override
+    public void initialize() {
+        int length = length();
+        for (int index = 0; index < length; index++) {
+            setBaseAt(index, (S) new DoubleBase(0.0));
+        }
+    }
+
+    @Override
+    public DoubleGene<T, S> copy() {
+        int length = length();
+
+        DoubleGene<T, S> copy = new DoubleGene<>(length);
+
+        for (int index = 0; index < length; index++) {
+            copy.setBaseAt(index, (S) getBaseAt(index).copy());
+        }
+
+        return copy;
+    }
+}
