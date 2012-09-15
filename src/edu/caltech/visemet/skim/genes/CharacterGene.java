8dc7abaa959801b35a512240b774f1caa5a14bfa
Max Hirschhorn <maxh@caltech.edu>
2012-09-14 17:29:48 -0700

    Created CharacterGene class.
    Initialized by setting each base as a new CharacterBase instance.

---
+++ src/edu/caltech/visemet/skim/genes/CharacterGene.java
@@ -0,0 +1,60 @@
+package edu.caltech.visemet.skim.genes;
+
+import edu.caltech.visemet.skim.AbstractGene;
+import edu.caltech.visemet.skim.Base;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @param <T> the type of value for bases of this gene
+ * @param <S> the type of base for this gene
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public class CharacterGene<
+        T extends Character,
+        S extends Base<T>
+> extends AbstractGene<T, S> {
+
+    /**
+     * Class constructor specifying the length.
+     *
+     * @param length the length of this gene
+     */
+    public CharacterGene(int length) {
+        super(new ArrayList<>(Collections.nCopies(length, (S) null)));
+    }
+
+    @Override
+    public void initialize() {
+        int length = length();
+        for (int index = 0; index < length; index++) {
+            setBaseAt(index, (S) new CharacterBase(CharacterBase.DIGITS, '0'));
+        }
+    }
+
+    @Override
+    public CharacterGene<T, S> copy() {
+        int length = length();
+
+        CharacterGene<T, S> copy = new CharacterGene<>(length);
+
+        for (int index = 0; index < length; index++) {
+            copy.setBaseAt(index, (S) getBaseAt(index).copy());
+        }
+
+        return copy;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (S base : getSequence()) {
+            sb.append(base);
+        }
+
+        return sb.toString();
+    }
+}
