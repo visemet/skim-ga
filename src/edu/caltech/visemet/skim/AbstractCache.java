437646623bec36ad7a54fa6e291b24e7291ba70f
Max Hirschhorn <maxh@caltech.edu>
2012-09-18 10:49:54 -0700

    Created AbstractCache class.
    Backed by a WeakHashMap so that when the instance is no longer used, it
    is removed from the cache. This is generally not how a cache works, but
    the purpose is to store the fitness values for members of the
    population, so once that population evolves those values are usually not
    used again.
    
    However, since it is possible to have the same chromosome (although new
    instance) exist from one population to the next, the put() method will
    actually replace the key stored in the WeakHashMap by removing it and
    then add an entry with the specified key and value.
    
    The AbstractCache class has no policy for how to handle a full cache,
    hence why it is abstract. In a future revision, an exception will be
    throw when trying to add to a full cache.

---
+++ src/edu/caltech/visemet/skim/AbstractCache.java
@@ -0,0 +1,82 @@
+package edu.caltech.visemet.skim;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+/**
+ *
+ * @author Max Hirschhorn #visemet
+ */
+public abstract class AbstractCache<K, V> implements Cache<K, V> {
+
+    private int maxSize;
+
+    private Map<K, V> map = new WeakHashMap<>();
+
+    public AbstractCache(int maxSize) {
+        if (maxSize < 1) {
+            throw new IllegalArgumentException("maximum size must be positive");
+        }
+
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    @Override
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean isFull() {
+        return (size() == getMaxSize());
+    }
+
+    @Override
+    public boolean contains(K key) {
+        return map.containsKey(key);
+    }
+
+    @Override
+    public V get(K key) {
+        return map.get(key);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        put(key, value, true);
+    }
+
+    private void put(K key, V value, boolean replaceKey) {
+        if (replaceKey) {
+            remove(key);
+        }
+
+        map.put(key, value);
+    }
+
+    @Override
+    public V remove(K key) {
+        return map.remove(key);
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public Map<K, V> asMap() {
+        return Collections.unmodifiableMap(map);
+    }
+}
