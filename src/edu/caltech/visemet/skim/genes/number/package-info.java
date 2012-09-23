8dbed73796bb1fb4669665fc6cdaa35b697e755d
Max Hirschhorn <maxh@caltech.edu>
2012-09-22 18:05:37 -0700

    Updated documentation.
    Added package-level comments to skim.genes and skim.genes.number
    packages.

---
+++ src/edu/caltech/visemet/skim/genes/number/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the classes that implement the {@code NumberBase} and {@code Gene}
+ * interfaces.
+ */
+package edu.caltech.visemet.skim.genes.number;
---
+++ src/edu/caltech/visemet/skim/genes/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides the classes that implement the {@code Base} and {@code Gene}
+ * interfaces.
+ */
+package edu.caltech.visemet.skim.genes;
